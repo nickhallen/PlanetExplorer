19b3bdda71f54f3f7a5ed8db1045a4f4f9a4127e
somename <someemail>
2015-10-22 13:28:48 +0300

    Project finished

--- src/org/unioulu/tol/sqat2015/planetExplorer/PlanetExplorer.java
+++ src/org/unioulu/tol/sqat2015/planetExplorer/PlanetExplorer.java
@@ -1,14 +1,32 @@
 package org.unioulu.tol.sqat2015.planetExplorer;
 
+import java.awt.Component;
+import java.awt.GridLayout;
+
+import javax.swing.JPanel;
+
 // Before submitting write your ID and finish time here. Your ID is written on project description sheets.
 // ID:
 // Finish time:
 public class PlanetExplorer {
 	public PlanetExplorer(int x, int y, String obstacles){
+		
+		PlanetExplorer explorer = new PlanetExplorer(100,100,"(5,5)(7,8)");
+		
+		JPanel panel = new JPanel(new GridLayout(100, 100)); 
+		PlanetExplorer [][] fields = new PlanetExplorer[100][100];
+		for(int i = 0; i<20; i++){
+		     for(int j = 0; j<20;j++){
+		          Component PlanetExplorer;
+				PlanetExplorer[i][j] = new PlanetExplorerj, j, obstacles;
+		          panel.add(PlanetExplorer[i][j]);      
+		     }
+		}
+		 
+		 
 	/*	x and y represent the size of the grid.
 	 *  Obstacles is a String formatted as follows: "(obs1_x,obs1_y)(obs2_x,obs2_y)...(obsN_x,obsN_y)" with no white spaces. 
 	 *  
-		Example use:
 		PlanetExplorer explorer = new PlanetExplorer(100,100,"(5,5)(7,8)")  //A 100x100 grid with two obstacles at coordinates (5,5) and (7,8) 
 	 */
 	}
